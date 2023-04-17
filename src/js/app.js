1f5a3686a900246d6d2d2f351041dccd57aa39e6
elmiramm <elmira.m.m@yandex.ru>
2023-04-17 10:33:10 +0300

    intro.html

--- src/js/app.js
+++ src/js/app.js
@@ -18,36 +18,78 @@ if (document.querySelector(".gallery__inner")) {
 // ------Swiper-------
 Swiper.use([Navigation, Pagination, Parallax, Autoplay]);
 
-if (document.querySelector('.slider-section__body')) {
-	new Swiper('.slider-section__body', {
+// if (document.querySelector('.slider-section__body')) {
+// 	new Swiper('.slider-section__body', {
+// 		observer: true, //обновляет swiper при изменении элементов слайдера
+// 		observeParents: true, //обновляет swiper при изменении в родительских элементах слайдера
+// 		slidesPerView: 1,
+// 		spaceBetween: 0,
+// 		// slidesPerGroup: 4, //если нужно чтобы кол-во dots было меньше кол-ва slides. Slides будут перелистываться группами
+// 		// centeredSlides: true, //Активный слайд по центру
+// 		loop: true, // pза последним слайдем будет идти заново первый
+// 		watchOverflow: false,
+// 		speed: 800,
+// 		loopAdditionalSlides: 5,
+// 		preloadImage: false,
+// 		// parallax: true,
+// 		simulateTouch: false, //вкл/выкл перетаскинавия на ПК
+// 		pagination: {
+// 			el: '.slider-section__controls .dots',
+// 			clickable: true, //true - можно кликнуть на dotts и перейти на нужный слайд
+// 			dynamicBullets: false, // ]true - dots будут менять размер в зависимости от того насколько далеко расположены от активного dots
+// 		},
+// 		// autoplay: {
+// 		// 	delay: 3000, //Пауза м.д. прокрутками
+// 		// 	stopOnLastSlide: false,
+// 		// 	disabledOnInteraction: false,
+// 		// },
+// 		effect: 'slide', //значение по умолчанию - перелистывает слайды, 'fade' - смена прозрачности
+// 		// navigation: {
+// 		// 	nextEl: '.slider-section .slider-arrow--next',
+// 		// 	prevEl: '.slider-section .slider-arrow--prev',
+// 		// },
+// 		// breakpoints: { //Mobile First - значения от заданной точки и больше 
+// 		// 	279: {
+// 		// 		slidesPerView: 1.1,
+// 		// 		spaceBetween: 15,
+// 		// 	},
+// 		// 	768: {
+// 		// 		slidesPerView: 2,
+// 		// 		spaceBetween: 20,
+// 		// 	},
+// 		// 	992: {
+// 		// 		slidesPerView: 3,
+// 		// 		spaceBetween: 32,
+// 		// 	},
+// 		// }
+// 	});
+// }
+
+// ------Swiper-End-------
+
+if (document.querySelector('.slider-intro__body')) {
+	new Swiper('.slider-intro__body', {
 		observer: true, //обновляет swiper при изменении элементов слайдера
 		observeParents: true, //обновляет swiper при изменении в родительских элементах слайдера
 		slidesPerView: 1,
 		spaceBetween: 0,
-		// slidesPerGroup: 4, //если нужно чтобы кол-во dots было меньше кол-ва slides. Slides будут перелистываться группами
-		// centeredSlides: true, //Активный слайд по центру
-		loop: true, // pза последним слайдем будет идти заново первый
+		loop: true, // за последним слайдем будет идти заново первый
 		watchOverflow: false,
 		speed: 800,
 		loopAdditionalSlides: 5,
 		preloadImage: false,
-		// parallax: true,
 		simulateTouch: false, //вкл/выкл перетаскинавия на ПК
 		pagination: {
-			el: '.slider-section__controls .dots',
+			el: '.slider-intro__controls .dots',
 			clickable: true, //true - можно кликнуть на dotts и перейти на нужный слайд
 			dynamicBullets: false, // ]true - dots будут менять размер в зависимости от того насколько далеко расположены от активного dots
 		},
-		// autoplay: {
-		// 	delay: 3000, //Пауза м.д. прокрутками
-		// 	stopOnLastSlide: false,
-		// 	disabledOnInteraction: false,
-		// },
+		autoplay: {
+			delay: 3000, //Пауза м.д. прокрутками
+			stopOnLastSlide: false,
+			disabledOnInteraction: false,
+		},
 		effect: 'slide', //значение по умолчанию - перелистывает слайды, 'fade' - смена прозрачности
-		// navigation: {
-		// 	nextEl: '.slider-section .slider-arrow--next',
-		// 	prevEl: '.slider-section .slider-arrow--prev',
-		// },
 		// breakpoints: { //Mobile First - значения от заданной точки и больше 
 		// 	279: {
 		// 		slidesPerView: 1.1,
@@ -64,9 +106,6 @@ if (document.querySelector('.slider-section__body')) {
 		// }
 	});
 }
-
-// ------Swiper-End-------
-
 window.onload = function () {
 	document.addEventListener('click', documentActions);
 
