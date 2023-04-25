65903b9a34ae2810ea38bd927b0192adf123c1b9
elmiramm <elmira.m.m@yandex.ru>
2023-04-25 17:49:58 +0300

    catalog-intro.html

--- src/js/app.js
+++ src/js/app.js
@@ -105,6 +105,52 @@ if (document.querySelector('.slider-intro__body')) {
 		// }
 	});
 }
+if (document.querySelector('.slider-catalog__body')) {
+	new Swiper('.slider-catalog__body', {
+		observer: true, //обновляет swiper при изменении элементов слайдера
+		observeParents: true, //обновляет swiper при изменении в родительских элементах слайдера
+		slidesPerView: 1,
+		spaceBetween: 0,
+		// slidesPerGroup: 4, //если нужно чтобы кол-во dots было меньше кол-ва slides. Slides будут перелистываться группами
+		// centeredSlides: true, //Активный слайд по центру
+		loop: true, // pза последним слайдем будет идти заново первый
+		watchOverflow: false,
+		speed: 800,
+		loopAdditionalSlides: 5,
+		preloadImage: false,
+		// parallax: true,
+		simulateTouch: true, //вкл/выкл перетаскинавия на ПК
+		// pagination: {
+		// 	el: '.slider-catalog__controls .dots',
+		// 	clickable: true, //true - можно кликнуть на dotts и перейти на нужный слайд
+		// 	dynamicBullets: false, // ]true - dots будут менять размер в зависимости от того насколько далеко расположены от активного dots
+		// },
+		// autoplay: {
+		// 	delay: 3000, //Пауза м.д. прокрутками
+		// 	stopOnLastSlide: false,
+		// 	disabledOnInteraction: false,
+		// },
+		effect: 'slide', //значение по умолчанию - перелистывает слайды, 'fade' - смена прозрачности
+		navigation: {
+			nextEl: '.slider-catalog .slider-arrow--next',
+			prevEl: '.slider-catalog .slider-arrow--prev',
+		},
+		// breakpoints: { //Mobile First - значения от заданной точки и больше 
+		// 	279: {
+		// 		slidesPerView: 1.1,
+		// 		spaceBetween: 15,
+		// 	},
+		// 	768: {
+		// 		slidesPerView: 2,
+		// 		spaceBetween: 20,
+		// 	},
+		// 	992: {
+		// 		slidesPerView: 3,
+		// 		spaceBetween: 32,
+		// 	},
+		// }
+	});
+}
 // ------Swiper-End---------------------------------------------------------------------
 
 
